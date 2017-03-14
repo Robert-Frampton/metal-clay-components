6b6cca76093ec46437b2bdad8a4677d17fd35898
Patrick Yeo <patrick.yeo@liferay.com>
2017-03-14 13:40:47 -0700

    Metal-quartz-dropdown tests

--- packages/metal-quartz-dropdown/test/MetalQuartzDropdown.js
+++ packages/metal-quartz-dropdown/test/MetalQuartzDropdown.js
@@ -1,34 +1,161 @@
 'use strict';
 
+import { async } from 'metal';
+import dom from 'metal-dom';
 import MetalQuartzDropdown from '../src/MetalQuartzDropdown';
 
-let dropdown;
-
 describe('MetalQuartzDropdown', function() {
-	afterEach(() => {
+	let dropdown;
+
+	afterEach(function() {
 		if (dropdown) {
 			dropdown.dispose();
 		}
 	});
 
-	it('should generate the content markup', function() {
-		dropdown = new MetalQuartzDropdown(
-			{
-				content: 'foo'
-			}
-		);
+	it('should generate link dropdown markup.', function() {
+		dropdown = new MetalQuartzDropdown({
+			dropdownToggle: {
+				component: 'link',
+				elementClasses: 'btn btn-link',
+				href: '#1',
+				label: 'Dropdown Toggle'
+			},
+
+			dropdownItems: [{
+				component: 'link',
+				href: '#1',
+				label: 'Dropdown Item 1'
+			}, {
+				component: 'link',
+				elementClasses: 'active',
+				href: '#1',
+				label: 'Dropdown Item 2'
+			}, {
+				component: 'link',
+				elementClasses: 'disabled',
+				href: '#1',
+				label: 'Dropdown Item 3'
+			}]
+		});
 
 		assert.strictEqual(dropdown.element.outerHTML, __html__['test/fixture/testDefaultMetalQuartzDropdown.html'].trim());
 	});
 
-	it('should append elementClasses to the outer element', function() {
-		dropdown = new MetalQuartzDropdown(
-			{
-				content: 'foo',
-				elementClasses: 'css-class'
-			}
-		);
+	it('should append elementClasses to the outer element.', function() {
+		dropdown = new MetalQuartzDropdown({
+			elementClasses: 'dropdown-wide',
+			dropdownToggle: {
+				component: 'link',
+				elementClasses: 'btn btn-link',
+				href: '#1',
+				label: 'Dropdown Toggle'
+			},
 
-		assert.strictEqual(dropdown.element.outerHTML, __html__['test/fixture/testElementClassesMetalQuartzDropdown.html'].trim());
+			dropdownItems: [{
+				component: 'link',
+				href: '#1',
+				label: 'Dropdown Item 1'
+			}]
+		});
+
+		assert(dom.hasClass(dropdown.element, 'dropdown-wide'));
+	});
+
+	it('should use li, p, span, div elements as the container for dropdown, respectively.', function() {
+		const elArray = ['li', 'p', 'span', 'div'];
+
+		for (const el of elArray) {
+			dropdown = new MetalQuartzDropdown({
+				dropdownParentElement: el,
+				dropdownToggle: {
+					component: 'link',
+					elementClasses: 'btn btn-link',
+					href: '#1',
+					label: 'Dropdown Toggle'
+				},
+
+				dropdownItems: [{
+					component: 'link',
+					href: '#1',
+					label: 'Dropdown Item 1'
+				}]
+			});
+
+			assert(dom.match(dropdown.element, el + '.dropdown'));
+
+			dropdown.dispose();
+		}
+	});
+
+	it('should default to div if dropdownParentElement is not defined.', function() {
+		dropdown = new MetalQuartzDropdown({
+			dropdownToggle: {
+				component: 'link',
+				elementClasses: 'btn btn-link',
+				href: '#1',
+				label: 'Dropdown Toggle'
+			},
+
+			dropdownItems: [{
+				component: 'link',
+				href: '#1',
+				label: 'Dropdown Item 1'
+			}]
+		});
+
+		assert.ok(dom.match(dropdown.element, 'div.dropdown'));
+	});
+
+	it('should add class `open` to dropdown when clicked', function(done) {
+		dropdown = new MetalQuartzDropdown({
+			dropdownToggle: {
+				component: 'link',
+				elementClasses: 'btn btn-link',
+				href: '#1',
+				label: 'Dropdown Toggle'
+			},
+
+			dropdownItems: [{
+				component: 'link',
+				href: '#1',
+				label: 'Dropdown Item 1'
+			}]
+		});
+
+		dom.triggerEvent(dropdown.element.querySelector('.btn.btn-link'), 'click');
+
+		async.nextTick(function() {
+			assert(dom.hasClass(document.querySelector('.dropdown'), 'open'));
+
+			done();
+		});
+	});
+
+	it('should add class `show` to dropdown when clicked', function(done) {
+		dropdown = new MetalQuartzDropdown({
+			dropdownOpenClass: 'show',
+
+			dropdownToggle: {
+				component: 'link',
+				elementClasses: 'btn btn-link',
+				href: '#1',
+				label: 'Dropdown Toggle'
+			},
+
+			dropdownItems: [{
+				component: 'link',
+				href: '#1',
+				label: 'Dropdown Item 1'
+			}]
+		});
+
+		dom.triggerEvent(dropdown.element.querySelector('.btn.btn-link'), 'click');
+
+		async.nextTick(() => {
+			assert(dom.hasClass(document.querySelector('.dropdown'), 'show'));
+
+			done();
+		});
 	});
 });
